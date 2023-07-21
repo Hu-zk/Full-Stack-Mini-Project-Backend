2dca3eade5e070fefcddfd0e438581af27800e3a
Huuzk <138253670+Hu-zk@users.noreply.github.com>
2023-07-21 09:05:55 +0300

    editing the register file

--- register.php
+++ register.php
@@ -1,25 +1,32 @@
 <?php
 include('connection.php');
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-$email = $_POST['email'];
 
-$check_email = $mysqli->prepare('select email from users where email=?');
-$check_email->bind_param('s', $email);
-$check_email->execute();
-$check_email->store_result();
-$email_exists = $check_email->num_rows();
+$json_data = file_get_contents('php://input');
+$data = json_decode($json_data, true);
 
-if ($email_exists == 0) {
-    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-    $query = $mysqli->prepare('insert into users(username,email,password) values(?,?,?)');
-    $query->bind_param('sss', $username,$email, $hashed_password);
-    $query->execute();
+if ($data !== null) {
+    $username = $data['username'];
+    $password = $data['password'];
+    $email = $data['email'];
 
-    $response['status'] = "success";
-} else {
-    $response['status'] = "failed";
+    $check_email = $mysqli->prepare('select email from users where email=?');
+    $check_email->bind_param('s', $email);
+    $check_email->execute();
+    $check_email->store_result();
+    $email_exists = $check_email->num_rows();
+
+    if ($email_exists == 0) {
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $query = $mysqli->prepare('insert into users(username,email,password) values(?,?,?)');
+        $query->bind_param('sss', $username,$email, $hashed_password);
+        $query->execute();
+    
+        $response['status'] = "success";
+        echo json_encode($response);
+    }
+    else{
+        $response['status'] = "failed";
+        echo json_encode($response);
+    }
 }
-
-echo json_encode($response);
